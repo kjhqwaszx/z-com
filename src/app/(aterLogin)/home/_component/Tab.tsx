d94748aa4be42c1f95215c0842ec53eab3ad9415
김재한 <kyobo.jaehan@gmail.com>
2024-01-01 20:50:36 +0900

    Home > Tab 개발

---
+++ src/app/(aterLogin)/home/_component/Tab.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import style from './tab.module.css'
+import {useState} from "react";
+
+export default function Tab(){
+    const [tab, setTab] = useState('rec');
+
+    const onClickRec = () => {
+        setTab('recommend');
+    }
+    const onClickFol = () => {
+        setTab('follow');
+    }
+
+    return (
+        <div className={style.homeFixed}>
+            <div className={style.homeText}>홈</div>
+            <div className={style.homeTab}>
+                <div onClick={onClickRec}>
+                    추천
+                    <div className={style.tabIndicator} hidden={tab === 'follow'}></div>
+                </div>
+                <div onClick={onClickFol}>
+                    팔로우 중
+                    <div className={style.tabIndicator} hidden={tab === 'recommend'}></div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/(aterLogin)/home/page.tsx
+++ src/app/(aterLogin)/home/page.tsx
@@ -1,8 +1,15 @@
+import style from './home.module.css'
+import Tab from "@/app/(aterLogin)/home/_component/Tab";
+import TabProvider from "@/app/(aterLogin)/home/_component/TabProvider";
+import PostForm from "@/app/(aterLogin)/home/_component/PostForm";
 
 export default function Home():JSX.Element{
     return (
-        <main>
-            홈페이지
+        <main className={style.main}>
+            <TabProvider>
+                <Tab/>
+                <PostForm/>
+            </TabProvider>
         </main>
     )
 }
