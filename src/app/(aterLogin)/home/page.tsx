e50df40ac780c104ca2fddf5f4ef13ca64a82d5f
김재한 <kyobo.jaehan@gmail.com>
2024-01-01 21:19:03 +0900

    Home > Post 개발

--- src/app/(aterLogin)/home/page.tsx
+++ src/app/(aterLogin)/home/page.tsx
@@ -2,6 +2,7 @@ import style from './home.module.css'
 import Tab from "@/app/(aterLogin)/home/_component/Tab";
 import TabProvider from "@/app/(aterLogin)/home/_component/TabProvider";
 import PostForm from "@/app/(aterLogin)/home/_component/PostForm";
+import Post from "@/app/(aterLogin)/_component/Post";
 
 export default function Home():JSX.Element{
     return (
@@ -9,6 +10,12 @@ export default function Home():JSX.Element{
             <TabProvider>
                 <Tab/>
                 <PostForm/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
             </TabProvider>
         </main>
     )
