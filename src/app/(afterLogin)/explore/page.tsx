a00f630f12cc7d42027f68376a3a3de9d365d983
김재한 <kyobo.jaehan@gmail.com>
2024-01-06 14:48:05 +0900

    Explore 메뉴 개발

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,26 @@
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import style from "@/app/(afterLogin)/explore/explore.module.css";
+import Trend from "@/app/(afterLogin)/_component/Trend";
+
 export default function Explore (){
     return(
-        <main>
-            탐색 페이지
+        <main className={style.main}>
+            <div className={style.formZone}>
+                <SearchForm />
+            </div>
+            <div className={style.trend}>
+                <h3>나를 위한 트렌드</h3>
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+            </div>
         </main>
     )
 }
--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -8,6 +8,7 @@ import NavMenu from "@/app/(afterLogin)/_component/NavMenu";
 import LogoutButton from "@/app/(afterLogin)/_component/LogoutButton";
 import TrendSection from "@/app/(afterLogin)/_component/TrendSection";
 import FollowRecommend from "@/app/(afterLogin)/_component/FollowRecommend";
+import RightSearchZone from "@/app/(afterLogin)/_component/RightSearchZone";
 
 type Props = {children: ReactNode, modal: ReactNode}
 
@@ -18,7 +19,7 @@ export default function AfterLoginLayout({children, modal}:Props){
             <header className={style.leftSectionWrapper}>
                 <section className={style.leftSection}>
                     <div className={style.leftSectionFixed}>
-                        <Link clasName={style.logo} href="/home">
+                        <Link className={style.logo} href="/home">
                             <div className={style.logoPill}>
                                 <Image src={zLogo} alt="z.com 로고" width={40} height={40}/>
                             </div>
@@ -33,22 +34,15 @@ export default function AfterLoginLayout({children, modal}:Props){
                     </div>
                 </section>
             </header>
-            {/*우측 메뉴*/}
             <div className={style.rightSectionWrapper}>
                 <div className={style.rightSectionInner}>
+                    {/*중앙 메인*/}
                     <main className={style.main}>{children}</main>
+                    {/*우측 영역*/}
                     <section className={style.rightSection}>
-                        <div style={{ marginBottom: 60, width: 'inherit' }}>
-                            <form className={style.search}>
-                                <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
-                                    <g>
-                                        <path
-                                            d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
-                                    </g>
-                                </svg>
-                                <input type="search" />
-                            </form>
-                        </div>
+                        {/*검색 창*/}
+                        <RightSearchZone/>
+                        {/*트렌드*/}
                         <TrendSection/>
                         <div className={style.followRecommend}>
                             <h3>팔로우 추천</h3>
