c5f7aafb5e65803fd3b7fdf6cd855380488dbd51
김재한 <kyobo.jaehan@gmail.com>
2024-01-07 15:23:36 +0900

    Mock Service Worker 설치 및 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import {MSWComponent} from "@/app/_component/MSWComponent";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({children}: Props) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <MSWComponent/>
           {children}
       </body>
     </html>
