32638d192b4409a333b827009efb6be4dfb2df23
jaehan <kyobo.jaehan@gmail.com>
2024-01-09 16:17:50 +0900

    홈 > 추천 & 팔로우 게시물 개발 ( react Query )

--- src/app/(afterLogin)/home/_component/FollowingPosts.tsx
+++ src/app/(afterLogin)/home/_component/FollowingPosts.tsx
@@ -1,17 +1,18 @@
 "use client"
 
 import {useQuery} from "@tanstack/react-query";
-import {getPostRecommend} from "@/app/(afterLogin)/home/_lib/getPostRecommends";
 import Post from "@/app/(afterLogin)/_component/Post";
 import {Post as IPost} from '@/model/Post'
 import React from "react";
+import {getFollowingPosts} from "@/app/(afterLogin)/home/_lib/getFollowingPosts";
 
-export default function PostRecommends(){
+export default function FollowingPosts(){
     const {data} = useQuery<IPost[]>({
-        queryKey:['posts', 'recommends'],
-        queryFn: getPostRecommend,
+        queryKey:['posts', 'followings'],
+        queryFn: getFollowingPosts,
         staleTime: 60*1000, //fresh -> stale (1분)
         gcTime: 300 * 1000
+
     })
 
     return  data?.map((post) => (
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -4,23 +4,22 @@ import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
 import {dehydrate, HydrationBoundary, QueryClient} from "@tanstack/react-query";
 import {getPostRecommend} from "@/app/(afterLogin)/home/_lib/getPostRecommends";
-import PostRecommends from "@/app/(afterLogin)/home/_component/PostRecommends";
+import TabDecider from "@/app/(afterLogin)/home/_component/TabDecider";
 
 
 export default async function Home(){
-    //  서버에서 불러온 데이터 (dehydratedState)를 HydrationBoundary 를 통해 클라이언트가 받는다 (hydrate)
     const queryClient = new QueryClient
     await queryClient.prefetchQuery({queryKey:['posts', 'recommends'], queryFn: getPostRecommend})
     const dehydratedState = dehydrate(queryClient)
 
     return (
         <main className={style.main}>
-            {/**/}
+            {/*서버 컴포넌트 이므로 서버 데이터(dehydratedState) 를 클라이언트로 Hydration 해주는 과정) */}
             <HydrationBoundary state={dehydratedState}>
                 <TabProvider>
                     <Tab/>
                     <PostForm/>
-                    <PostRecommends/>
+                    <TabDecider/>
                 </TabProvider>
             </HydrationBoundary>
         </main>
