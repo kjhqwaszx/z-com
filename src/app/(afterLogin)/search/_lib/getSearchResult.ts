e9ecd06ae057f2bf0722c92434457cc40c5fa05a
jaehan <kyobo.jaehan@gmail.com>
2024-01-16 17:04:43 +0900

    주소창에 # 인코딩 시키기

--- src/app/(afterLogin)/search/_lib/getSearchResult.ts
+++ src/app/(afterLogin)/search/_lib/getSearchResult.ts
@@ -4,8 +4,8 @@ import {Post} from "@/model/Post";
 export const getSearchResult: QueryFunction<Post[], [_1: string, _2: string, searchParams: { q: string, pf?: string, f?: string }]>
     = async ({ queryKey }) => {
   const [_1, _2, searchParams] = queryKey;
-  // const urlSearchParams = new URLSearchParams(searchParams);
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/search/${searchParams.q}?${searchParams.toString()}`, {
+  const urlSearchParams = new URLSearchParams(searchParams);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts?${urlSearchParams.toString()}`, {
     next: {
       tags: ['posts', 'search', searchParams.q],
     },
